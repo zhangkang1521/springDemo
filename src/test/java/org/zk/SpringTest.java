fc563bd7deba3bd40a747e4bc5564841fa21b6f4
zhangkang <zhangkang_pub@163.com>
2018-04-20 11:02:42 +0800

    spring test

--- src/test/java/org/zk/SpringTest.java
+++ src/test/java/org/zk/SpringTest.java
@@ -1,30 +1,28 @@
 package org.zk;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.zk.beans.User;
 import org.zk.config.ApplicationConfig;
 
 /**
  * Created by Administrator on 8/27/2016.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {ApplicationConfig.class})
 public class SpringTest {
 
-    // xml配置
-    @Test
-    public void testXml() {
-        ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
-        User user = (User)ctx.getBean("user");
-        System.out.println(user);
-    }
+    @Autowired
+    User user;
 
-    // Java注解配置
     @Test
-    public void testAnnotation() {
-        ApplicationContext ctx = new AnnotationConfigApplicationContext(ApplicationConfig.class);
-        User user = ctx.getBean(User.class);
+    public void test1() {
         System.out.println(user);
     }
 }
