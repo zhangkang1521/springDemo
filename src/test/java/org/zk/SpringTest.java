a59a41dec0614df0a42086c419c5e6f7a64d4b18
zhangkang <zhangkang@lvmama.com>
2019-03-09 22:14:11 +0800

    集成ActiveMQ，使用jmsTemplate

--- src/test/java/org/zk/SpringTest.java
+++ src/test/java/org/zk/SpringTest.java
@@ -4,27 +4,39 @@ import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
 import org.zk.beans.User;
 import org.zk.config.ApplicationConfig;
+
+import javax.jms.*;
 
 /**
  * Created by Administrator on 8/27/2016.
  */
 public class SpringTest {
 
-    // xml配置
     @Test
-    public void testXml() {
+    public void testSend() {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
-        User user = (User)ctx.getBean("user");
-        System.out.println(user);
+        JmsTemplate jmsTemplate = ctx.getBean("jmsTemplate", JmsTemplate.class);
+        Destination destination = ctx.getBean("destination", Destination.class);
+        jmsTemplate.send(destination, new MessageCreator() {
+            @Override
+            public Message createMessage(Session session) throws JMSException {
+                return session.createTextMessage("hello,world");
+            }
+        });
     }
 
-    // Java注解配置
     @Test
-    public void testAnnotation() {
-        ApplicationContext ctx = new AnnotationConfigApplicationContext(ApplicationConfig.class);
-        User user = ctx.getBean(User.class);
-        System.out.println(user);
+    public void testReceive() throws Exception{
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+        JmsTemplate jmsTemplate = ctx.getBean("jmsTemplate", JmsTemplate.class);
+        Destination destination = ctx.getBean("destination", Destination.class);
+        TextMessage textMessage = (TextMessage) jmsTemplate.receive(destination);
+        System.out.println(textMessage.getText());
     }
+
+
 }
